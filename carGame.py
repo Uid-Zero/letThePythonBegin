f97ddeac815713b6aec9e84103ac6e6dde2485e1
Sai Kolavennu <SaiKolavennu.Linux@gmail.com>
2022-01-18 21:36:50 -0600

    Making use of functions - CarGame.

--- carGame.py
+++ carGame.py
@@ -5,6 +5,17 @@ exitCar = False
 
 # Sample code to test the workings of If/While loops. You can start and stop the engine or get the current status of the car. Use [Help] to take a look at the allowed commands.
 
+def help():
+    print("""
+        Please use below commands to use the engine:
+
+        Start   :   To start the engine.
+        Stop    :   To stop the engine.
+        Exit    :   To exit the car.
+        Help    :   To repeat this menu.
+        Status  :   Print the current status of the engine.
+        """)
+
 while not exitCar:
     engineCommand = input("Enter an Engine Command [Help]: ").lower()
     if engineCommand == "start":
@@ -24,14 +35,5 @@ while not exitCar:
         exitCar = True
     elif engineCommand == "status":
         print(f"Current status of the engine: {engineStatus}")
-    elif engineCommand == "help":
-        print("""
-        Please use below commands to use the engine:
-        Start   :   To start the engine.
-        Stop    :   To stop the engine.
-        Exit    :   To exit the car.
-        Help    :   To repeat this menu.
-        Status  :   Print the current status of the engine.
-        """)
     else:
-        print("Invalid Command, Please use [Help] to print the valid options. ")
+        help()
