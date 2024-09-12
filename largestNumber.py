056f69ed929804bb823e247647a424924d7b8501
Sai Kolavennu <Sai.Kolavennu@iCloud.com>
2024-09-12 18:51:58 -0500

    Input the list from users instead of hard coding.

--- largestNumber.py
+++ largestNumber.py
@@ -1,10 +1,20 @@
 # Find the largest number in a given list
 
-list = [1, 354, 2154, 2135, 235, 324, 354, 987, 231, 0]
-largestNumber = list[0]
+try: 
+# Ask user for input
+    user_input = input("Enter numbers separated by commas: ")
 
-for number in list:
-    if number > largestNumber:
-        largestNumber = number
+    # Convert input string into a list of integers
+    list = [int(x.strip()) for x in user_input.split(',')]
 
-print(largestNumber)
+    if len(list) == 0:
+        print("The list is empty. ")
+    else:
+        largestNumber = list[0]
+        for number in list:
+            if number > largestNumber:
+                largestNumber = number
+        print(largestNumber)
+
+except ValueError:
+    print("Please only enter valid int values.")
